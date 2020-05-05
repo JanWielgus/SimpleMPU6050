1cd84f7998e6774ec6ab78e9f931e49599796ce0
JanWielgus <jan.wielgus12@gmail.com>
2020-05-05 14:46:30 +0200

    Update FC_MPU6050Lib.h

--- FC_MPU6050Lib.h
+++ FC_MPU6050Lib.h
@@ -10,12 +10,7 @@
 #ifndef _FC_MPU6050LIB_h
 #define _FC_MPU6050LIB_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
-#else
-	#include "WProgram.h"
-#endif
-
+#include "arduino.h"
 #include <Wire.h>
 
 
